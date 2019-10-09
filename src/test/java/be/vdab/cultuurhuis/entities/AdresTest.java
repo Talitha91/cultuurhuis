975e5149bd040057243721b9a95e158d8a0a2e75
Jeroen Debra <jeroendebra@gmail.com>
2019-10-09 16:01:04 +0200

    testtoegevoegd

--- src/test/java/be/vdab/cultuurhuis/entities/AdresTest.java
+++ src/test/java/be/vdab/cultuurhuis/entities/AdresTest.java
@@ -1,7 +1,35 @@
 package be.vdab.cultuurhuis.entities;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class AdresTest {
+
+    private Adres adres;
+    private Validator validator;
+
+    @Before
+    public void before(){
+        adres = new Adres();
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void straatMagNietLeegZijn(){
+       adres.setStraat("");
+
+        validator.validateProperty(adres, "straat");
+    }
 
 }
